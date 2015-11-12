ef84bab882d99a5f661015c3f44028a8c4751637
Adrian Corscadden <a.corscadden@gmail.com>
2015-11-12 06:36:41 -0800

    Initial commit

---
+++ fixXcodePluginUUIDs.sh
@@ -0,0 +1,2 @@
+XCODEUUID=`defaults read /Applications/Xcode.app/Contents/Info DVTPlugInCompatibilityUUID`
+for f in ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins/*; do defaults write "$f/Contents/Info" DVTPlugInCompatibilityUUIDs -array-add $XCODEUUID; done
